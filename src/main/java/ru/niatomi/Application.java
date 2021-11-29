ee9cd006b24a73b02fd776f8738e41dd1f657a28
Niatomi <playervoker@gmail.com>
2021-11-30 02:00:56 +0400

    Added making of car lists and fileApi

--- src/main/java/ru/niatomi/Application.java
+++ src/main/java/ru/niatomi/Application.java
@@ -1,38 +1,54 @@
 package ru.niatomi;
 
 import lombok.SneakyThrows;
+import ru.niatomi.annotation.ParseIndex;
+//import ru.niatomi.api.FileApi;
+import ru.niatomi.api.FileApi;
 import ru.niatomi.models.Car;
 import ru.niatomi.models.CarMaker;
 import ru.niatomi.processor.ParseIndexProcessor;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     @SneakyThrows
     public static void main(String[] args) {
-//        Path path = Paths.get(App.class
-//                .getClassLoader()
-//                .getResource("CAR_DATA.csv")
-//                .toURI());
-//        BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-//        String data = br.readLine() + "\r\n";
-//        while (br.readLine() != null) {
-//            data += br.readLine()+"\r\n";
-//        }
-//        System.out.println(data);
+        //init
+        ParseIndexProcessor processor = new ParseIndexProcessor();
+        Path path = Paths.get(Application.class
+                .getClassLoader()
+                .getResource("CAR_DATA.csv")
+                .toURI());
+
+        BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+
+        //register class
+        String header = br.readLine();
+        processor.registerClass(header, Car.class);
+
+        //Car list
+        List<Car> cars = new ArrayList<Car>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            cars.add(processor.parseObject(line, Car.class));
+        }
+        br.close();
 
 
-//        ParseIndexProcessor parseIndexProcessor = new ParseIndexProcessor();
-//        Car car = parseIndexProcessor.parseObject("Town Car,Lincoln,2007,Orange", Car.class);
-//        System.out.println(car);
+//        File writing
+        FileApi fileApi = new FileApi();
+        fileApi.writeFile(cars, "CAR_DATA_dump.csv");
 
-//        ParseIndexProcessor parseIndexProcessor = new ParseIndexProcessor();
-//        CarMaker carMaker = parseIndexProcessor.parseObject("Town Car,Lincoln,2007,Orange", CarMaker.class);
-//        System.out.println(carMaker);
-
+        System.out.println();
     }
 }
