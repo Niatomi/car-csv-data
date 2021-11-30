5852ab0a628864f69e110d927cb8b5a3f05c64ad
Niatomi <playervoker@gmail.com>
2021-11-30 08:10:07 +0400

    #Altered csv file access - directly editing without using of target field
    #Remade access to data via Map
    
    #Added writing via FileIO

--- src/main/java/ru/niatomi/Application.java
+++ src/main/java/ru/niatomi/Application.java
@@ -1,54 +1,57 @@
 package ru.niatomi;
 
 import lombok.SneakyThrows;
-import ru.niatomi.annotation.ParseIndex;
 //import ru.niatomi.api.FileApi;
-import ru.niatomi.api.FileApi;
+import ru.niatomi.api.FileIO;
 import ru.niatomi.models.Car;
-import ru.niatomi.models.CarMaker;
 import ru.niatomi.processor.ParseIndexProcessor;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     @SneakyThrows
     public static void main(String[] args) {
         //init
+        FileIO fileIO = new FileIO();
         ParseIndexProcessor processor = new ParseIndexProcessor();
-        Path path = Paths.get(Application.class
-                .getClassLoader()
-                .getResource("CAR_DATA.csv")
-                .toURI());
+        Path path = Paths.get("")
+                .toAbsolutePath()
+                .resolve("src")
+                .resolve("main")
+                .resolve("resources")
+                .resolve("CAR_DATA.csv");
 
+        //Saving data from file and clearing data on csv
         BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-
-        //register class
+        Map<String, Integer> map = fileIO.readFile();
+        System.out.println(map.toString().indexOf(2));
         String header = br.readLine();
+        br.close();
+//        System.out.println(map.entrySet());
+        //register class
         processor.registerClass(header, Car.class);
 
-        //Car list
         List<Car> cars = new ArrayList<Car>();
-        String line;
-        while ((line = br.readLine()) != null) {
-            cars.add(processor.parseObject(line, Car.class));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            System.out.println(entry.getValue());;
         }
-        br.close();
+
+        //Car list
+//        List<Car> cars = new ArrayList<Car>();
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            cars.add(processor.parseObject(entry.getKey(), Car.class));
+//        }
 
 
-//        File writing
-        FileApi fileApi = new FileApi();
-        fileApi.writeFile(cars, "CAR_DATA_dump.csv");
+        //File writing
+        fileIO.writeFile(cars, "CAR_DATA_dump.csv");
 
-        System.out.println();
     }
 }
