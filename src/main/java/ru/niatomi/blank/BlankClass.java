0bb46ed98529fcf74974c0c7534cd7c703d293d4
Niatomi <playervoker@gmail.com>
2021-12-01 12:57:01 +0400

    BlankClass commit

--- src/main/java/ru/niatomi/blank/BlankClass.java
+++ src/main/java/ru/niatomi/blank/BlankClass.java
@@ -18,7 +18,31 @@ public class BlankClass {
     @SneakyThrows
     public static void main(String[] args) {
 
-    List<Integer> a = Stream.of(1,2,3,4).skip(2).collect(Collectors.toList());
+
+//        List<Integer> a = Stream.of(1,2,3,4).skip(2).collect(Collectors.toList());
+//
+//        Stream<String> lines = Files.lines(path);
+//        Path path = Paths.get("")
+//                .toAbsolutePath()
+//                .resolve("src")
+//                .resolve("main")
+//                .resolve("resources")
+//                .resolve("CAR_DATA.csv");
+//        Map<String, Integer> bag = Files.lines(path, StandardCharsets.UTF_8)
+//                .map(str -> str.replaceAll("[,\\.\\-]", " "))
+//                .map(str -> str.replaceAll("\\s+", " "))
+//                .map(String::trim)
+//                .map(String::toLowerCase)
+//                .flatMap(line -> Arrays.stream(line.split("\\s+")))
+//                .collect(Collectors.groupingBy(str -> str))
+//                .entrySet()
+//                .stream()
+//                .collect(Collectors.toMap(Map.Entry::getKey, ent -> ent.getValue().size()));
+//
+//        for (Map.Entry<String, Integer> entry : bag.entrySet()) {
+//            System.out.println(entry);
+//        }
+
         Path path = Paths.get("")
                 .toAbsolutePath()
                 .resolve("src")
@@ -26,24 +50,33 @@ public class BlankClass {
                 .resolve("resources")
                 .resolve("CAR_DATA.csv");
 
-        Stream<String> lines = Files.lines(path);
+        List<String> list_example = new ArrayList<String>();
+        list_example.add("123");
+        list_example.add("456");
+        list_example.add("789");
 
-        Map<String, Integer> bag = Files.lines(path, StandardCharsets.UTF_8)
-                .map(str -> str.replaceAll("[,\\.\\-]", " "))
+        List<String> bag =
+                Files.lines(path, StandardCharsets.UTF_8)
+                .map(str -> str.replaceAll("(,)\\1+", ",Not stated,"))
                 .map(str -> str.replaceAll("\\s+", " "))
                 .map(String::trim)
-                .map(String::toLowerCase)
-                .flatMap(line -> Arrays.stream(line.split("\\s+")))
-                .collect(Collectors.groupingBy(str -> str))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, ent -> ent.getValue().size()));
+                .flatMap(line -> Arrays.stream(line.split("\\n+")))
+                .collect(Collectors.toList());
+        System.out.println(bag.get(0));
 
-        for (Map.Entry<String, Integer> entry : bag.entrySet()) {
-            System.out.println(entry);
-        }
+        Stream stream = list_example.stream();
 
-
+//        Map<String, Integer> bag =
+//                        list_example
+//                        .stream()
+////                .filter(i -> i
+//                        .map(str -> str.replaceAll("(,)\\1+", ",Not stated,"))
+//                        .map(str -> str.replaceAll("\\s+", " "))
+//                        .map(String::trim)
+//                        .flatMap(line -> Arrays.stream(line.split("\\n+")))
+//                        .collect(Collectors.groupingBy(str -> str))
+//                        .entrySet()
+//                        .stream().collect(Collectors.groupingBy(Map.Entry::getKey,  ))
 
     }
 }
