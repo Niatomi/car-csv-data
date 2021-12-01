a5ca070ca953bf78acde196df379c2d9a2b900c4
Niatomi <playervoker@gmail.com>
2021-12-01 15:05:12 +0400

    Added deleting data from CAR_DATA.csv Swaped fileWriters Relised Car to CarMaker transaction Added sorting of manufacturers

--- src/main/java/ru/niatomi/Application.java
+++ src/main/java/ru/niatomi/Application.java
@@ -4,14 +4,16 @@ import lombok.SneakyThrows;
 //import ru.niatomi.api.FileApi;
 import ru.niatomi.api.FileIO;
 import ru.niatomi.models.Car;
+import ru.niatomi.models.CarMaker;
 import ru.niatomi.processor.ParseIndexProcessor;
 
+import java.lang.reflect.Array;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Application {
     @SneakyThrows
@@ -26,25 +28,60 @@ public class Application {
                 .resolve("resources")
                 .resolve("CAR_DATA.csv");
 
-        //Saving data from file and clearing data on csv
+        //3. Saving data from file and clearing data on csv
         List<String> bag = fileIO.readFile();
         String header = bag.get(0);
         bag.remove(0);
 
+        //4. clearing data
+        Files.deleteIfExists(path);
+        Files.createFile(path);
+
         //register class
         processor.registerClass(header, Car.class);
 
-        //Car objects
+        //5. Car objects
         List<Car> cars = new ArrayList<Car>();
         for (String el: bag) cars.add(processor.parseObject(el, Car.class));
+        fileIO.writeFile(cars, "CAR_DATA_OBJECTS.csv");
 
-        //Sorting car by color
+        //6. Sorting car by color
         Map<String, List<Car>> colorGroup = cars
                 .stream()
                 .collect(Collectors.groupingBy(Car::getCarColor));
-
-        //File writing
-        fileIO.writeFile(cars, "CAR_DATA_OBJECTS.csv");
         fileIO.writeFile(colorGroup.entrySet(), "CAR_DATA_COLOR_GROUPING.csv");
+
+        //7. Car to CarMaker
+        //7.1
+        Map<String, List<Car>> carMakers = cars
+                .stream()
+                .collect(Collectors.groupingBy(Car::getManufacturer));
+        //7.2
+        List<CarMaker> carMakerList = new ArrayList<CarMaker>();
+        for (Map.Entry<String, List<Car>> el : carMakers.entrySet()) {
+            carMakerList.add(new CarMaker(el.getKey(), el.getValue()));
+        }
+        //7.3
+        fileIO.writeFile(carMakerList, "MANUFACTURER_DATA.csv");
+        //7.4
+        String[] carManufacturers = carMakers.keySet().toArray(new String[0]);
+        System.out.println(Arrays.stream(carManufacturers).collect(Collectors.joining(",")));
+
+        //8.1 Sorting of manufacturers
+        Map<String, List<Car>> sortManufacturers =
+                carMakers
+                .entrySet()
+                .stream()
+                .filter(x -> x.getValue().size() > 2)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        //8.2
+        List<String> keys = new ArrayList<String>(sortManufacturers
+                .keySet()
+                .stream()
+                .sorted()
+                .collect(Collectors.toList()));
+        //8.3
+        fileIO.writeFile(keys, "SORTED_MANUFACTURES.csv");
+
     }
 }
